e9319967e55386bdb4a1fbeff458246636a9d957
jacarezin <luizfellipe230@gmail.com>
2024-01-26 18:19:33 -0300

    Error handling in mobile devices

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback";
+import "core-js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
